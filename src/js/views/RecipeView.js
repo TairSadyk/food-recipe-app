80baedcb0fc31317ba35d314156d894e27256dd8
TairSadyk <tairsadykov92@gmail.com>
2022-05-22 20:08:39 -0400

    added fracty

--- src/js/views/RecipeView.js
+++ src/js/views/RecipeView.js
@@ -1,7 +1,6 @@
 import View from "./View.js";
 import icons from "url:../../img/icons.svg";
-// import { Fraction } from "fractional";
-// import fracty from "fracty";
+import fracty from "fracty";
 class RecipeView extends View {
   _parentElement = document.querySelector(".recipe");
   // #data;
@@ -125,13 +124,13 @@ class RecipeView extends View {
   }
 
   _generateMarkupIngredient(ingredient) {
-    // const fracty = require("fracty");
-    // ${ingredient.quantity ? fracty(ingredient.quantity) : ""}
     return `<li class="recipe__ingredient">
     <svg class="recipe__icon">
     <use href="${icons}#icon-check"></use>
     </svg>
-    <div class="recipe__quantity">${ingredient.quantity}</div>
+    <div class="recipe__quantity">${
+      ingredient.quantity ? fracty(ingredient.quantity) : ""
+    }</div>
     <div class="recipe__description">
       <span class="recipe__unit">${ingredient.unit}</span>
       ${ingredient.description}
